c71fca526a25eb46b79eb9861d79fa6424807eb3
Kohei <1lovehamuhamu.15@gmail.com>
2025-12-11 22:45:16 -0800

    Initial Commit

---
+++ TegakiRecipe/TegakiRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TegakiRecipeApp.swift
+//  TegakiRecipe
+//
+//  Created by Kohei Sawabe on 2025-12-11.
+//
+
+import SwiftUI
+
+@main
+struct TegakiRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
