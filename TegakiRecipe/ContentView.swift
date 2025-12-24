d936170ef361c5ac93251e97e80a0ba8e10ba0b1
Kohei <1lovehamuhamu.15@gmail.com>
2025-12-24 02:58:01 -0800

    Implemented upload button and photo picker features

--- TegakiRecipe/ContentView.swift
+++ TegakiRecipe/ContentView.swift
@@ -5,19 +5,68 @@
 //  Created by Kohei Sawabe on 2025-12-11.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
+    @State var selectedPhotos: [PhotosPickerItem] = []
+    @State var images = [UIImage]()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Project updated")
+            ScrollView(.horizontal){
+                LazyHGrid(rows: [GridItem(.fixed(300))]){
+                    ForEach(0..<images.count, id: \.self){
+                        index in Image(uiImage: images[index])
+                            .resizable()
+                            .scaledToFit()
+                    }
+                }
+            }.onChange(of: selectedPhotos){
+                convertDataToImage()
+            }
+            PhotosPicker("Select a photo",
+                         selection: $selectedPhotos,
+                         maxSelectionCount: 1,
+                         selectionBehavior: .ordered,
+                         matching: .images
+            )
         }
         .padding()
+        Button {
+            Task {
+                await uploadPhotos(selectedPhotos)
+            }
+        } label: {
+            HStack(spacing: 8) {
+                Image(uiImage: UIImage(resource: .upload))
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 24, height: 24)
+                Text("Upload Recipe")
+            }
+        }
+    }
+    func convertDataToImage(){
+        images.removeAll()
+        
+        if !selectedPhotos.isEmpty{
+            for eachItem in selectedPhotos{
+                Task{
+                    if let imageData = try? await
+                        eachItem.loadTransferable(type: Data.self) {
+                        if let image = UIImage(data: imageData){
+                            images.append(image)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    func uploadPhotos(_ items: [PhotosPickerItem]) async{
+        
     }
 }
+
 
 #Preview {
     ContentView()
